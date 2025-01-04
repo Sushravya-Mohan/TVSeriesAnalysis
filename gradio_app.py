64d2e2321ac4593713d0184c948127168e247fa2
Sushravya-Mohan <67335303+Sushravya-Mohan@users.noreply.github.com>
2025-01-04 21:49:02 +0530

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -1,31 +1,21 @@
 import gradio as gr
+import pandas as pd
 from theme_classifier import ThemeClassifier
 
 def get_themes(theme_list_str, subtitles_path, save_path):
+    # Process input themes
     theme_list = theme_list_str.split(',')
     theme_classifier = ThemeClassifier(theme_list)
     output_df = theme_classifier.get_themes(subtitles_path, save_path)
 
-    # Remove dialogue from theme list
-    theme_list = [theme for theme in theme_list if theme!= 'dialogue']
+    # Remove 'dialogue' from themes and sum scores
+    theme_list = [theme for theme in theme_list if theme != 'dialogue']
     output_df = output_df[theme_list]
-
-    output_df = output_df[theme_list].sum().reset_index()
+    output_df = output_df.sum().reset_index()
     output_df.columns = ['Theme', 'Score']
 
-    output_chart = gr.BarPlot(
-        output_df,
-        x="Theme",
-        y="Score",
-        title="Series Themes",
-        tooltip=["Theme", "Score"],
-        vertical=False,
-        width=500,
-        height=260
-    )
-
-    return output_chart
-
+    # Return the data for updating BarPlot
+    return output_df
 
 def main():
     with gr.Blocks() as iface:
@@ -34,14 +24,30 @@ def main():
                 gr.HTML("<h1>Theme Classification (Zero Shot Classifiers)</h1>")
                 with gr.Row():
                     with gr.Column():
-                        plot = gr.BarPlot()
+                        # Define BarPlot component
+                        plot = gr.BarPlot(
+                            label="Theme Scores",
+                            x="Theme",
+                            y="Score",
+                            title="Series Themes",
+                            tooltip=["Theme", "Score"],
+                            vertical=False,
+                            width=500,
+                            height=260,
+                        )
                     with gr.Column():
+                        # Define input components
                         theme_list = gr.Textbox(label="Themes")
                         subtitles_path = gr.Textbox(label="Subtitles or Script Path")
                         save_path = gr.Textbox(label="Save Path")
                         get_themes_button = gr.Button("Get Themes")
-                        get_themes_button.click(get_themes, inputs=[theme_list, subtitles_path, save_path], 
-                                                outputs=[plot])
+
+                        # Link button click to the function
+                        get_themes_button.click(
+                            get_themes, 
+                            inputs=[theme_list, subtitles_path, save_path], 
+                            outputs=[plot]
+                        )
         
     iface.launch(share=True)
 
